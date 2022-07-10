1c0afceef2d491d49bcbd1f0a9ad9ef71c60ec54
Dmitry Lazarev <skydim@gmail.com>
2022-07-10 16:43:08 +0200

    refactor: tail параметр убран

--- cmd/dlog/main.go
+++ cmd/dlog/main.go
@@ -15,7 +15,8 @@ const VERSION = "1.0.0"
 func main() {
 	showVersion := false
 
-	flag.BoolVar(&config.Config.Enabled, "debug", false, "Enables debug messages, written to /tmp/dlog.log")
+	flag.BoolVar(&config.Config.Enabled, "debug", false,
+		"Enables debug messages, written to /tmp/dlog.log")
 	flag.BoolVar(&showVersion, "version", false, "Print version information")
 	flag.Parse()
 
