e2d7c0f3dc8600cea0e9dae79b9fb597da64646f
dimcz <skydim@gmail.com>
2022-07-12 13:03:17 +0200

    docs: корректная версия

--- cmd/dlog/main.go
+++ cmd/dlog/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/dimcz/dlog/utils"
 )
 
-const VERSION = "1.0.1"
+const VERSION = "1.0.2"
 
 func main() {
 	showVersion := flag.Bool("version", false, "Print version information")
