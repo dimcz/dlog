28089ea24e141f39cb98fd745f0cbf318e1c53d9
Dmitry Lazarev <skydim@gmail.com>
2022-07-12 21:26:13 +0200

    feat: удаление временной отметки из логов

--- fetcher.go
+++ fetcher.go
@@ -2,6 +2,7 @@ package dlog
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -151,9 +152,15 @@ func (f *Fetcher) seek(offset Offset) {
 	f.lineReaderOffset = offset
 }
 
+// stripTimeFromLine remove time stamp from beginning of line
+func (f *Fetcher) stripTimeFromLine(b []byte) []byte {
+	return b[bytes.IndexByte(b, ' ')+1:]
+}
+
 // reads and returns one Line, position and error, which can only be io.EOF, otherwise panics
 func (f *Fetcher) readline() ([]byte, Offset, error) {
 	str, err := f.lineReader.ReadBytes('\n')
+	str = f.stripTimeFromLine(str)
 	startingOffset := f.lineReaderOffset
 	if len(str) > 0 {
 		if err == nil {
