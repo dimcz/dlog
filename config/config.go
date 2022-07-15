f978129bf61f261084d5551d2e705eddff8b8e48
Dmitry Lazarev <skydim@gmail.com>
2022-07-15 20:24:49 +0200

    feat: уточнены параметры по умолчанию

--- config/config.go
+++ config/config.go
@@ -13,8 +13,8 @@ var values Config
 
 func init() {
 	flag.BoolVar(&(values.Version), "version", false, "Print version information")
-	flag.IntVar(&(values.Tail), "tail", 50_000, "Number of lines to show from the end of the logs")
-	flag.BoolVar(&(values.NoLoad), "noload", false, "Disable loading previous logs")
+	flag.IntVar(&(values.Tail), "tail", 1_000, "Number of lines to show from the end of the logs")
+	flag.BoolVar(&(values.NoLoad), "noload", true, "Disable loading previous logs")
 	flag.Int64Var(&(values.TimeShift), "shift", 24*60*60, "time chunk to download logs")
 	flag.Parse()
 }
